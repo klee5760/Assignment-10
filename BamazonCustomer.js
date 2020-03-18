efa67fd56b7443ca80976160872bf42cd9c441ab
Kevin Lee <klee5760@hotmail.com>
2020-03-17 18:37:25 -0600

    fifth commit

--- BamazonCustomer.js
+++ BamazonCustomer.js
@@ -1,7 +1,7 @@
 //require mysql and inquirer
 var inquirer = require("inquirer");
 var mysql = require("mysql");
-
+​
 //create connection to db
 var connection = mysql.createConnection({
   host: "localhost",
@@ -10,87 +10,90 @@ var connection = mysql.createConnection({
   password: "root",
   database: "bamazon"
 });
-
+​
 //connect method to accept callback function or display an error
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) throw err;
   console.log("Connection ID: " + connection.threadId);
 });
-
+​
 //display inventory
 function displayInventory() {
   //print the items for sale and their details
-  connection.query("SELECT * From Products", function(err, res) {
+  connection.query("SELECT * From Products", function (err, res) {
     if (err) throw err;
-
+​
     console.log("*******************");
     console.log("WELCOME TO BAMAZON!");
     console.log("**VIEW OUR ITEMS!**");
     console.log("*******************");
-
+​
     for (i = 0; i < res.length; i++) {
       //console logging the items and and a seperator bar; note that the resopnse requests
       //match the table column in my schema
-
+​
       console.log(
         "Item ID: " +
-          res[i].item_id +
-          "Product Name: " +
-          res[i].product_name +
-          "Department Name: " +
-          res[i].department_name +
-          "Price: " +
-          res[i].price +
-          "Stock Quantity: " +
-          res[i].stock_quantity
+        res[i].item_id +
+        "Product Name: " +
+        res[i].product_name +
+        "Department Name: " +
+        res[i].department_name +
+        "Price: " +
+        res[i].price +
+        "Stock Quantity: " +
+        res[i].stock_quantity
       );
-
+​
       console.log("*******************");
     }
     placeOrder();
   });
 }
-
-//place order
-//promts users to enter the ID of the product
+​
+// //place order
+// //promts users to enter the ID of the product
 function placeOrder() {
-  inquirer.prompt([{
-        type: "input",
-        name: "id",
-        message: "What is the ID of the product?",
-
-        validate: function(value) {
-          var valid = value.match(/^[0-9]+$/)
-          if(valid){
-            return true;
-          } 
-            return 'Enter a valid Product ID';
-          }
-        },
+  console.log('Inside place order');
+  inquirer.prompt([
     {
-        type: "input",
-        name: "quantity",
-        message: "How much do you want to purchase?",
+      type: "input",
+      name: "id",
+      message: "What is the ID of the product?",
+​
+      validate: function (value) {
+        var valid = value.match(/^[0-9]+$/)
+        if (valid) {
+          return true;
+        }
+        return 'Enter a valid Product ID';
+      }
+    },
+    {
+      type: "input",
+      name: "quantity",
+      message: "How much do you want to purchase?",
+​
+      validate: function (value) {
+        var valid = value.match(/^[0-9]+$/)
+        if (valid) {
+          return true;
+        }
+        return 'Enter a valid quantity';
+      }
+​
+    }
+  ])
+    .then(function (answer) {
+      console.log(answer);
+      connection.query('SELECT * FROM products WHERE item_id= ?;', 
+      [answer.id],
+      function (err, res) {
+        console.log(typeof answer.quantity);
+        if (answer.quantity > res[0].stock_quantity) {
+          console.log("Insufficient Quantity");
+          console.log("This order has been canceled");
 
-        validate: function(value) {
-          var valid = value.match(/^[0-9]+$/)
-          if(valid){
-            return true;
-          } 
-            return 'Enter a valid quantity';
-          }
-      
-    }])
-
-    //Logic to check the stock_quantity in MySQL and return a response
-    .then(function(answer) {
-        connection.query('Select * from products where id= ?',[answer.id],function(err, res) {
-
-
-//If the quantity is greater than stock_quantity
-            if (answer.selectQuantity > res[0].stock_quantity) {
-              console.log("Insufficient Quantity");
-              console.log("This order has been canceled");
               console.log("");
 
               newOrder();
@@ -118,7 +121,7 @@ function placeOrder() {
               newOrder();
             }
           }
-      },
+},
       function(err, res) {}
     );
 }
